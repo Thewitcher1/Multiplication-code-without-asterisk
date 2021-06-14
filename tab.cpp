7d4a5592698f4029ef472c7d4433efe4e317cae7
Thewitcher1 <36190319+Thewitcher1@users.noreply.github.com>
2021-06-14 19:22:06 +0200

    Update tab.cpp

--- tab.cpp
+++ tab.cpp
@@ -23,3 +23,64 @@ int main()
     cout <<multiply(3,4) <<endl;
     cout <<multiply(3,-4) <<endl;
 }
+
+
+---------------------------------------------------------------------------------
+  résolution d'équation différentielle:
+   
+#include<iostream>
+#include<cmath>
+
+
+void Fermeture()
+{
+    std::cout << "Au revoir" << std::endl;
+    system("pause");
+}
+
+int main()
+{
+    int A,B,C;
+    std::cout << "Entrer votre equetion Ax2 + Bx + C = 0" << std::endl;
+    std::cout << "Entrer la valeur de A" << std::endl;
+    std::cin>> A;
+    std::cout << "Entrer la valeur de B" << std::endl;
+    std::cin>> B;
+    std::cout << "Entrer la valeur de C" << std::endl;
+    std::cin>> C;
+    std:: cout << "L'equation à résoudre est : " << A<<"x2"<<"+"<<B<<"x"<<"+"<<C<< "= 0"<< std::endl;
+    float D = B * B - 4 * A * C;
+    if(A == 0)
+    {
+        float w = (- C)/B;
+        std::cout << " La solution est: " << w << std::endl;
+        Fermeture();
+        return 0;
+    }
+    if(D > 0)
+    {
+        float x = sqrt(D);
+        float y = (-B - x)/(2*A);
+        float z = (-B + x)/(2*A);
+        std::cout << "La solution est: " <<"x1 = " << y << " " << "x2 = "<< z << std::endl;
+        Fermeture();
+        return 0;
+    }
+    if(D == 0)
+    {
+        float w = -B/2*A;
+        std::cout << "La solution est: " << w << std::endl;
+        Fermeture();
+        return 0;
+    }
+    if(D < 0)
+    {
+        std::cout << "Votre equation ne posséde de solution dans R" << std::endl;
+        Fermeture();
+        return 0;
+    }
+
+} 
+   
+    
+   
